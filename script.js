b06e90631c0aa07e71f05e76f1543e8d82cd6103
Tyler <tyler@T-Marks-iMac-4.local>
2016-05-03 14:43:33 -0400

    Added angular controller

--- script.js
+++ script.js
@@ -1,18 +1,71 @@
-// My Crappy JS Skills :/
+$(function() {
+  $(".sign-up").click(function() {
+    //Expand button
+    $(".button").addClass("expanded");
+    //Hide sign up
+    $(".sign-up").addClass("hidden");
+    //Show button
+    $("button").removeClass("hidden");
+    // Show sign up form
+    $("form").toggleClass("hidden");
+  })
 
-$(".sign-up").on('click', function() {
-  $(".button").addClass("expanded");
-  $(".sign-up").addClass("hidden");
-  $(".content").addClass("background");
-  $("button").removeClass("hidden");
-  $("form").toggleClass("hidden");
-})
+  $("button").click( function() {
+    //Change button to google material fab
+    $(this).addClass("fab")
+    //Hide form
+    $("form").addClass("hidden");
+    $(".button").addClass("full");
+    $(".text").remove();
+    //Show header and dummy data
+    $(".header").removeClass("hidden");
+    $(".list-content").removeClass("hidden");
+  })
 
-$("button").on('click', function() {
-  $(this).addClass("fab");
-  $("form").addClass("hidden");
-  $(".button").addClass("full");
-  $(".text").remove();
-  $(".header").removeClass("hidden");
-})
 
+});
+//Angular controller for dummy data
+angular.module('demoThree', ['ngMaterial'])
+        .config(function($mdIconProvider) {
+            $mdIconProvider
+                .iconSet('communication', 'img/icons/sets/communication-icons.svg', 24);
+        })
+        .controller('demoThreeCtrl', function($scope) {
+
+            $scope.heroes = [{
+                //face : imagePath,
+                realName: 'Bruce Wayne',
+                name: 'Batman',
+                location: "Gotham City"
+            }, {
+                //face : imagePath,
+                realName: 'Clark Kent',
+                name: 'Superman',
+                location: "Metropolis"
+            }, {
+                //face : imagePath,
+                realName: 'Oliver Queen',
+                name: 'Green Arrow',
+                location: "Star City"
+            }, {
+                //face : imagePath,
+                realName: 'Barry Allen',
+                name: 'The Flash',
+                location: "Central City"
+            }, {
+                //face : imagePath,
+                realName: 'Arthur Curry',
+                name: 'Aquaman',
+                location: "Atlantis"
+            }, {
+                //face : imagePath,
+                realName: 'Dick Grayson',
+                name: 'Nightwing',
+                location: "Bludhaven"
+            }, {
+                //face : imagePath,
+                realName: 'Diana Prince',
+                name: 'Wonder Woman',
+                location: "Gateway City"
+            }, ];
+        });
